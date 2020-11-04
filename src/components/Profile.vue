514a620cd3368c8958f2c5f1c92c03f3f0917268
red1k <erdenebayar_sh@yahoo.com>
2020-11-04 12:38:09 +0800

    Vue project

---
+++ src/components/Profile.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="profile">
+
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Profile',
+		props: { }
+	}
+</script>
+
+<style scoped>
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <Profile />
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import Profile from '@/components/Profile.vue'
+
+export default {
+  name: 'Home',
+  components: {
+	Profile
+  }
+}
+</script>
